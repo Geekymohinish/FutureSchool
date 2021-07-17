0eb3d0aca5a35b53e5f5313a636d23c2d0dbf184
mohinishkumar <mohinishkumar@192.168.0.103>
2021-07-17 23:29:27 +0530

    updated basis insertion into db

--- db_connection/src/db_connection/JdbcConnection.java
+++ db_connection/src/db_connection/JdbcConnection.java
@@ -8,9 +8,10 @@ public class JdbcConnection {
 		String url = "jdbc:mysql://localhost:3306/nightly_run";
 		String user = "root";
 		String pwd = "rootroot";
-	
-		String query = "select * from component";
+		
 		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String query = "select * from component";
 		Connection con = DriverManager.getConnection(url, user, pwd);
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(query);
@@ -19,6 +20,18 @@ public class JdbcConnection {
 			System.out.print(rs.getString("sno")+' ');
 			System.out.println(rs.getString("compname"));
 		}
+		
+		query = "insert into component values(3,'fpd')";
+		st.executeUpdate(query);
+		
+		query = "select * from component";
+		rs = st.executeQuery(query);
+	
+		while(rs.next()) {
+			System.out.print(rs.getString("sno")+' ');
+			System.out.println(rs.getString("compname"));
+		}
+		
 		st.close();
 		con.close();
 	}
